b3c0021d63a90833832689d6ca07a7e213bd050f
Allen Zhang <azhang9328@gmail.com>
2019-12-05 23:16:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,6 +9,12 @@ def map(array)
   newarray
 end   
 
-def reduce(array)
-  
+def reduce(array, starting_point)
+  i = 0
+  total = 0 
+  while i < array.length do
+    yield(array[i])
+    i += 1
+  end
+  total 
 end   
