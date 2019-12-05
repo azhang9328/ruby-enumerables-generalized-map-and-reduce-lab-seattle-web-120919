65e6d9745e27560bbdefd39fbe40abe49e006a6c
Allen Zhang <azhang9328@gmail.com>
2019-12-05 23:10:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,14 +1,5 @@
 # Your Code Here
 def map(array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    yield 
-    i += 1
-  end
-  new
+
 end   
 
-test(array) do |origarray|
-  new.push( source_array[i] * -1 )
-end   
