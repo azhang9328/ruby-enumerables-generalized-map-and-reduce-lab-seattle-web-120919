6c7f1bf8f0463f619e1ad7aab0bdcb6810f74131
Allen Zhang <azhang9328@gmail.com>
2019-12-05 23:11:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,11 @@
 # Your Code Here
 def map(array)
-   new = []
-   i 
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 ) # <== Unique work
+    i += 1
+  end
+  return new
 end   
 
